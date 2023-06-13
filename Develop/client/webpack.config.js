bf76efbb0d8696953824b4b5418af967df39157d
lexslie <lesliiee727@gmail.com>
2023-06-12 20:52:27 -0700

    update webpack.config.js
    added workbox plugins, css loaders, and babel

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -23,11 +21,48 @@ module.exports = () => {
         template: "./index.html",
         title: "J.A.T.E."
       }),
+      // workbox plugins for service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // workbox plugins for manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "text-editor",
+        short_name: "J.A.T.E.",
+        description: "Just Another Text Editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [{
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        }],
+      }),
     ],
 
     module: {
+      // adding css loaders and babel to webpack
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["styl-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presents: ["@babel.present-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            },
+          },
+        },
       ],
     },
   };
